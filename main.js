d36667c82cc35ef5d7d46c07360616091ef5b466
Sean <jb@JBs-MacBook-Pro.local>
2021-01-26 20:36:34 -0600

    .forEach practice, do not merge into main

---
+++ main.js
@@ -0,0 +1,12 @@
+// .forEach
+
+const array = [1,2,3,4,5,6];
+
+array.forEach((item) => {
+  console.log(item, i)
+})
+
+// FOR LOOP
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
